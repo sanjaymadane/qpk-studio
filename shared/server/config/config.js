1f021856622961b97c0e86ac97cdadf6ead06cea
Sanjay Madane <Sanjay Madane>
2016-09-02 18:12:14 +0800

    mongo config updated

--- shared/server/config/config.js
+++ shared/server/config/config.js
@@ -3,14 +3,12 @@
 /*
  * Application configuration file
  */
-console.log(process.env.QCONTACTZ_SERVER_IP + "test");
 var SERVER_IP = process.env.QCONTACTZ_SERVER_IP || '172.17.30.151';
-console.log(SERVER_IP);
 module.exports = {
   'secret': 'crazyalarm1',
-  'database': 'mongodb://'+SERVER_IP+':27018/qpk',
+  'database': 'mongodb://'+SERVER_IP+':27025/qpk',
   'api_version': 'v1',
   'token_timeout': 900000,
   'server_ip': SERVER_IP,
-  'port': 9090
+  'port': 9010
 };
