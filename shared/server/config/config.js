1ce284a341be3d6e0ae7b1c1aecac2fe3ada3c3a
Sanjay Madane <Sanjay Madane>
2016-09-02 20:12:19 +0800

    shell script executer added

--- shared/server/config/config.js
+++ shared/server/config/config.js
@@ -3,7 +3,7 @@
 /*
  * Application configuration file
  */
-var SERVER_IP = process.env.QCONTACTZ_SERVER_IP || '172.17.30.151';
+var SERVER_IP = process.env.QCONTACTZ_SERVER_IP || '172.17.30.95';
 module.exports = {
   'secret': 'crazyalarm1',
   'database': 'mongodb://'+SERVER_IP+':27025/qpk',
